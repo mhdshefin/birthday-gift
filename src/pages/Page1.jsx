ecca7dd0bd543145f5f6ae472fd92acdf0be31a9
mhdshefin <rishnabinthsalam@gmail.com>
2026-02-09 14:10:59 +0530

    updated button issue

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,13 +1,27 @@
 import "../App.css";
 import { useNavigate } from "react-router-dom";
 import cat5 from "../assets/cat5.webm";
+import { useState } from "react";
 
 function Page1() {
   const navigate = useNavigate();
 
+  const [pos, setPos] = useState({ top: 0, left: 0 });
+  const [escaped, setEscaped] = useState(false);
+
+  const moveButton = () => {
+    const maxX = window.innerWidth - 140;
+    const maxY = window.innerHeight - 80;
+
+    const randomLeft = Math.random() * maxX;
+    const randomTop = Math.random() * maxY;
+
+    setPos({ top: randomTop, left: randomLeft });
+    setEscaped(true);
+  };
+
   return (
     <div className="valentine-bg">
-      {/* hearts */}
       {[...Array(15)].map((_, i) => (
         <span key={i} className={`heart h${i + 1}`}></span>
       ))}
@@ -25,11 +39,26 @@ function Page1() {
           playsInline
         />
 
-        <div className="btn-group">
-          <button className="btn yes" onClick={() => navigate("/page3")}>
-            NO
+        {/* button area */}
+        <div className="container">
+          <button
+            className="btn yes"
+            style={
+              escaped
+                ? {
+                    position: "fixed",
+                    top: pos.top,
+                    left: pos.left,
+                  }
+                : {}
+            }
+            onMouseEnter={moveButton}   // desktop
+            onClick={moveButton}        // mobile
+          >
+            YES
           </button>
-           <button className="btn yes" onClick={() => navigate("/page3")}>
+
+          <button className="btn no" onClick={() => navigate("/page3")}>
             NO
           </button>
         </div>
